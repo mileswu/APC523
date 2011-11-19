1e094a8dbc0ec04e5f145c066cbe7215770ffc12
Miles Wu <miles.wu@wu-home.co.uk>
2011-11-19 21:37:06 +0000

    .

--- HW3/question1.cc
+++ HW3/question1.cc
@@ -6,15 +6,13 @@ using namespace std;
 #include "numericalrecipes/rk4.h"
 #include "numericalrecipes/bsstep.h"
 
-void euler_iterate(double *x, double *y, double *v_x, double *v_y, double timestep) {
-	double prev_v_x = *v_x;
-	double prev_v_y = *v_y;
+void euler_iterate(double t, double *y, int n, double timestep, void (*deriv)(double, double *, double *)) {
+	double dydt[n];
+	deriv(t, y, dydt);
 
-	*v_x -= 1.0 / pow(*x * *x + *y * *y, 1.5) * *x * timestep;
-	*v_y -= 1.0 / pow(*x * *x + *y * *y, 1.5) * *y * timestep;
-
-	*x += prev_v_x * timestep;
-	*y += prev_v_y * timestep;
+	for(int i=0; i<n; i++) {
+		y[i] += dydt[i] * timestep;
+	}
 }
 
 void deriv_func(double t, double y[], double dydt[]) {
@@ -28,39 +26,68 @@ void deriv_offsetcorrected(double t, double y[], double dydt[]) {
 	deriv_func(t, y+1, dydt+1); 
 }
 
+double angularmomentum(double y[]) {
+	double l = 0;
+	l += y[3]*y[0];
+	l += -y[2]*y[1];
+	return l;
+}
+
+double energy(double y[]) { 
+	double energy = 0;
+	energy += 0.5 * y[2]*y[2] + 0.5 * y[3]*y[3];
+	energy -= 1.0 / sqrt(y[0]*y[0] + y[1]*y[1]);
+	return energy;
+}
+
 void output(ofstream &f, double t, double *y) {
 	f << t << " ";
 	f << y[0] << " " << y[1] << " ";
-	f << y[2] << " " << y[3] << " ";
+	f << energy(y) << " ";
+	f << angularmomentum(y);
+
 	f << endl;
 }
 
+
+void init(double *y) {
+	/* Circular */
+	y[0] = 1;
+	y[1] = 0;
+	y[2] = 0;
+	y[3] = 1;
+
+	/* Eliptical */
+}
+
 int main() {
-	double y[4] = {1, 0, 0, 1}; //0=x, 1=y, 2=v_x, 3=v_y
+	double y[4]; //0=x, 1=y, 2=v_x, 3=v_y
 	double dydt[4];
-	double timestep = 0.01;
+	double timestep = 0.3;
+	double max = 3.141592654*2*1000;
 	
 	ofstream f;
-	//f.open("q1-euler.csv");
-
-	/*for(double t=0; t<60; t+=timestep) {
-		euler_iterate(&x, &y, &v_x, &v_y, timestep);
-		output(&f, t, x, y, v_x, v_y);
+	f.open("q1-euler.csv");
+	init(y);
+	for(double t=0; t<max; t+=timestep) {
+		output(f, t, y);
+		euler_iterate(t, y, 4, timestep, deriv_func);
 	}
-	f.close();*/
+	f.close();
 
 	f.open("q1-rk4.csv");
-	for(double t=0; t<10; t+=timestep) {
-		deriv_func(t, y, dydt);
+	init(y);
+	for(double t=0; t<max; t+=timestep) {
 		output(f, t, y);
+		deriv_func(t, y, dydt);
 		rk4(y-1, dydt-1, 4, t, timestep, y-1, deriv_offsetcorrected);
 	}
 	f.close();
 	
 	f.open("q1-bs.csv");
-	timestep = 0.01;
+	init(y);
 	double timestep_did;
-	for(double t=0; t<10;) {
+	for(double t=0; t<max;) {
 		deriv_func(t, y, dydt);
 		output(f, t, y);
 		double yscal[5] = {1, 1, 1, 1, 1};
@@ -72,7 +99,6 @@ int main() {
 				yscal,
 				&timestep_did, &timestep_todo,
 				deriv_offsetcorrected);
-		cout << "timestep: " << timestep_did << endl;
 	}
 	f.close();
 
